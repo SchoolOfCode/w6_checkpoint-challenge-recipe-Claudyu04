c3cd661ff608dd3b2b74480e68c526fbae34e080
Claudyu04 <81428418+Claudyu04@users.noreply.github.com>
2021-05-07 19:37:54 +0100

    receipe generator

--- main.js
+++ main.js
@@ -8,7 +8,16 @@ function handleFoodChange() {
   foodToSearch = document.querySelector("#food-input").value;
 }
 
+const YOUR_APP_ID = "eecc661d"
+const YOUR_APP_KEY = "bf4bf82e13005c603841071bd3edcde0"
+
+const aTag = document.getElementById("recipe-label");
+
 async function fetchRecipe(food) {
-  //--- write your code below ---
-  //--- write your code above ---
+  const requestUrl = await fetch (`https://api.edamam.com/search?q=${food}&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}`);
+  const dataResponse = await requestUrl.json();
+  // console.log (dataResponse);
+ const resultRecipe = dataResponse.hits[0];
+ aTag.innerHTML = resultRecipe.recipe.label;
+ aTag.href = resultRecipe.recipe.url;
 }
