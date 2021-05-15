8c363f8d943ff0bf620d09996c93aa75b27f8823
Claudyu04 <81428418+Claudyu04@users.noreply.github.com>
2021-05-15 18:24:13 +0100

    modified

--- main.js
+++ main.js
@@ -1,61 +1,41 @@
 let foodToSearch = null;
-const recipeImage1 = document.getElementById("image-id1");
-const recipeImage2 = document.getElementById("image-id2");
-const recipeImage3 = document.getElementById("image-id3");
-const recipeCaption1 = document.getElementById("caption1");
-
 function handleRecipeClick() {
-  fetchRecipe(foodToSearch);
-}
-
+    fetchRecipe(foodToSearch);
+ }
 function handleFoodChange() {
   foodToSearch = document.querySelector("#food-input").value;
 }
-
 const YOUR_APP_ID = "eecc661d"
 const YOUR_APP_KEY = "bf4bf82e13005c603841071bd3edcde0"
+const searchForm = document.querySelector('form');
+const searchResultSection = document.getElementById('search-result');
+const recipeSection = document.getElementById('recipe-section');
 
-const aTag = document.getElementById("recipe-label");
-
-async function fetchRecipe(food) {
-  const requestUrl = await fetch (`https://api.edamam.com/search?q=${food}&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}`);
-  const dataResponse = await requestUrl.json();
-  // console.log (dataResponse);
- const resultRecipe = dataResponse.hits[0];
- aTag.innerHTML = resultRecipe.recipe.label;
- aTag.href = resultRecipe.recipe.url;
+ async function fetchRecipe() {
+   //use fetch to do GET request for recipe 
+   const requestUrl = await fetch(`https://api.edamam.com/search?q=${foodToSearch}&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}&to=3`);
+   //console.log(requestUrl);
+   const dataResponse = await requestUrl.json();
+   // only grabs the hits array from the response
+   generateRecipeResults(dataResponse.hits);
+   console.log(dataResponse);
+ }
+function generateRecipeResults(recipes){
+ let generatedResults = '';   
+ recipes.map(result => {
+  generatedResults += 
+  `
+    <div class="recipe-data">
+        <img class="recipe-img" src="${result.recipe.image}" alt="" />
+        <div class="item-result">
+            <h2 class="title">${result.recipe.label}</h2>
+            <a class="recipe-label" href="${result.recipe.url}" target="_blank">View Recipe</a>
+        </div>
+        <p class="recipe-data">Calories : ${result.recipe.calories.toFixed(1)}</p>
+        <p class="recipe-data">Diet label : ${result.recipe.dietLabels}</p>
+        <p class="recipe-data"> Ingredients: ${result.recipe.ingredientLines}</p>
+    </div>
+  `
+})
+searchResultSection.innerHTML = generatedResults;
 }
-
-// Can you add images, ingredients or other information to your page?
-async function fetchRecipe(food) {
- // show first receipe image
-  const requestUrl = await fetch (`https://api.edamam.com/search?q=${food}&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}`);
-  const dataResponse = await requestUrl.json();
-  // console.log (dataResponse);
- const resultRecipe = dataResponse.hits[0];
- recipeImage1.src = resultRecipe.recipe.image;
-recipeImage1.href = resultRecipe.recipe.url;
-// aTag.innerHTML = resultRecipe.recipe.label;
-recipeCaption1.innerHTML = resultRecipe.recipe.label;
- // show second receipe image
-const requestUrl2 = await fetch (`https://api.edamam.com/search?q=${food}&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}`);
-  const dataResponse2 = await requestUrl2.json();
-  const resultRecipe2 = dataResponse2.hits[1];
- aTag.href = resultRecipe.recipe.url;
- recipeImage2.src = resultRecipe2.recipe.image;
- recipeImage2.href = resultRecipe2.recipe.url;
-// show third receipe image
- const requestUrl3 = await fetch (`https://api.edamam.com/search?q=${food}&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}`);
-  const dataResponse3 = await requestUrl3.json();
-  const resultRecipe3 = dataResponse3.hits[2];
- aTag.href = resultRecipe.recipe.url;
- recipeImage3.src = resultRecipe3.recipe.image;
- recipeImage3.href = resultRecipe3.recipe.url;
-}
-
-// const todoIdList = [1, 2, 3, 4]
-// for (const id of todoIdList) {
-//   const response = await fetch(`https://jsonplaceholder.typicode.com/todos/${id}`)
-//   const todo = await response.json()
-//   console.log(todo.title)
-// }
